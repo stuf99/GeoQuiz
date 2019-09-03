c916a1db6101a1120d1fe02af79e85bd0d35a4e8
stuf99 <stuf99@gmail.com>
2019-09-03 22:00:14 +0200

    New Activity

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -35,7 +35,7 @@ public class QuizActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate(Bundle) called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-    //    mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
